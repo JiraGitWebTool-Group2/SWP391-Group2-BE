c9ce63bfd926fc7f5a39c44fbf9db82bdaf61c33
Hai124-coder <haihenry1124@gmail.com>
2026-02-11 01:21:38 +0700

    Structure

---
+++ WebTool-BE/SWP391.Group2.Domain/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace SWP391.Group2.Domain
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ WebTool-BE/SWP391.Group2.Infrastructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace SWP391.Group2.Infrastructure
+{
+    public class Class1
+    {
+
+    }
+}
