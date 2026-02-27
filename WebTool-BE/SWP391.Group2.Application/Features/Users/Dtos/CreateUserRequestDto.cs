74d7c3816030be6931526e4b4f9b63f45e175910
unknown <zdainguyenz@gmail.com>
2026-02-28 04:53:52 +0700

    API add User into Database

---
+++ WebTool-BE/SWP391.Group2.Application/Features/Users/Dtos/CreateUserRequestDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWP391.Group2.Application.Features.Users.Dtos
+{
+    public class CreateUserRequestDto
+    {
+        [Required, EmailAddress, StringLength(255)]
+        public string Email { get; set; } = default!;
+
+        [Required, StringLength(200)]
+        public string FullName { get; set; } = default!;
+
+        // LOCAL thì bắt buộc có Password
+        [StringLength(100, MinimumLength = 6)]
+        public string? Password { get; set; }
+
+        // LOCAL/GOOGLE/GITHUB
+        [StringLength(30)]
+        public string? Provider { get; set; }
+
+        // GOOGLE: sub, GITHUB: id/login (tuỳ bạn lưu gì)
+        [StringLength(255)]
+        public string? ProviderUserId { get; set; }
+    }
+}
---
+++ WebTool-BE/SWP391.Group2.Application/Features/Users/Dtos/UserDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWP391.Group2.Application.Features.Users.Dtos
+{
+    public class UserDto
+    {
+        public int UserId { get; set; }
+        public string Email { get; set; } = default!;
+        public string FullName { get; set; } = default!;
+        public string? Provider { get; set; }
+        public string? ProviderUserId { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
