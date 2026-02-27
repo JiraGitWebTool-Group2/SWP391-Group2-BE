f90da83df310fe8c0c3ebd818df41ec1d7ebcc38
Hai124-coder <haihenry1124@gmail.com>
2026-02-27 23:52:47 +0700

    Add Get Sync Run

---
+++ WebTool-BE/SWP391.Group2.Application/Features/Sync/Queries/GetSyncRunStatusHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SWP391.Group2.Application.Abstractions;
+using SWP391.Group2.Application.Features.Sync.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWP391.Group2.Application.Features.Sync.Queries
+{
+    public class GetSyncRunStatusHandler : IRequestHandler<GetSyncRunStatusQuery, SyncRunStatusDto>
+    {
+        private readonly IApplicationDbContext _db;
+        public GetSyncRunStatusHandler(IApplicationDbContext db) => _db = db;
+
+        public async Task<SyncRunStatusDto> Handle(GetSyncRunStatusQuery request, CancellationToken ct)
+        {
+            var run = await _db.SyncRuns.AsNoTracking()
+                .FirstOrDefaultAsync(x => x.SyncRunId == request.SyncRunId, ct);
+
+            if (run is null) throw new KeyNotFoundException("SyncRun not found.");
+
+            return new SyncRunStatusDto(
+                run.SyncRunId,
+                run.RunStatus,
+                run.Notes,
+                run.StartedAt,
+                run.FinishedAt
+            );
+        }
+    }
+}
---
+++ WebTool-BE/SWP391.Group2.Application/Features/Sync/Queries/GetSyncRunStatusQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using SWP391.Group2.Application.Features.Sync.Dtos;
+
+
+namespace SWP391.Group2.Application.Features.Sync.Queries
+{
+    public record GetSyncRunStatusQuery(int SyncRunId) : IRequest<SyncRunStatusDto>;
+}
