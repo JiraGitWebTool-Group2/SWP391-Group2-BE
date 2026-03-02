182c99c5279baac266b7089fe14f6d3509d10233
Hai124-coder <haihenry1124@gmail.com>
2026-03-02 12:51:40 +0700

    GET snapshot traceability

---
+++ WebTool-BE/SWP391.Group2.Application/Features/Snapshots/Queries/GetSnapshotTraceabilityLiteQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using SWP391.Group2.Application.Features.Snapshots.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWP391.Group2.Application.Features.Snapshots.Queries
+{
+    public sealed record GetSnapshotTraceabilityLiteQuery(
+        int SnapshotId,
+        string? ProjectKeyPrefix = null, // optional: "SWP391" để filter key
+        int Top = 50                    // lấy top N issue keys
+    ) : IRequest<SnapshotTraceabilityLiteDto>;
+}
