157cea2e9422feaebd948cc0fa1dc74595bdc8ef
Hai124-coder <haihenry1124@gmail.com>
2026-03-02 15:35:04 +0700

    Setup Jira Config

---
+++ WebTool-BE/SWP391.Group2.Application/Abstractions/Jira/IJiraClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWP391.Group2.Application.Abstractions.Jira
+{
+    public record JiraIssueDto(
+        string IssueKey,
+        string Summary,
+        string? Description,
+        string IssueType,
+        string Priority,
+        string Status,
+        decimal? StoryPoints,
+        string Url,
+        string? AssigneeAccountId
+    );
+
+    public interface IJiraClient
+    {
+        Task<IReadOnlyList<JiraIssueDto>> SearchIssuesAsync(
+            string baseUrl,
+            string jql,
+            int maxResults,
+            string token,
+            CancellationToken ct
+        );
+    }
+}
